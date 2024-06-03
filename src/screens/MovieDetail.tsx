a9e4d0807e82bf2246fd18795a25db374b8597e5
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-06-03 23:24:02 +0700

    03/06/2024

---
+++ src/screens/MovieDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const MovieDetail = () => {
+  return (
+    <View>
+      <Text>Movie Detail Screen</Text>
+    </View>
+  )
+}
+
+export default MovieDetail
